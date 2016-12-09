bc7825c8a9602c5b17599838acb0b4e406337ec0
Gaurav Soni <stevejobs@Gauravs-Mac.local>
2016-12-09 17:20:15 +0530

    React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-import logo from './logo.svg';
-import './App.css';
-import './animate.css';
+import './Style/App.css';
+import './Style/Animate.css';
 
 class App extends Component {
-
   constructor(){
     super();
     this.state={
       todo:[],
-      editing:false,
       value: ''
     };
   };
 
+/*Adding Todo on keypress= enter*/
   entertodo(keypress){
     var Todo=this.refs.inputodo.value;
     if( keypress.charCode == 13 )
-
     {
       this.setState({
         todo: this.state.todo.concat({Value:Todo, checked:false})
@@ -26,6 +23,7 @@ class App extends Component {
       this.refs.inputodo.value=null;
     };
   };
+
   todo(todo,i){
     return (
       <li>
@@ -36,80 +34,45 @@ class App extends Component {
           </div>
           <div className="item">
             <span className="destroy" onClick={this.remove.bind(this, i)}>X</span>
-            <button onClick={this.edit.bind(this,i)} className="editmode">edit</button>
           </div>
         </div>
       </li>
     );
   };
 
+  /*Todo Delete*/
   remove(i){
     this.state.todo.splice(i,1)
     this.setState({todo:this.state.todo})
   };
+
+  /*Striking Todo on completion*/
   todoCompleted(i){
-     var todo=this.state.todo;
-     todo[i].checked =todo[i].checked? false:true;
-       this.setState({
-         todo:this.state.todo
-       });
-   };
+    var todo=this.state.todo;
+    todo[i].checked =todo[i].checked? false:true;
+    this.setState({
+      todo:this.state.todo
+    });
+  };
+
+  /*Mark all completed*/
   allCompleted=()=>{
     var todo = this.state.todo;
     var _this = this
     todo.forEach(function(item) {
       item.className = _this.state.finished ? "newtodo" : "line"
       item.checked = !_this.state.finished
-    })
+    });
     this.setState({todo: todo, finished: !this.state.finished})
   };
-  changeValue(e) {
-    this.setState({
-    value: this.state.value = e.target.value
-  });
-}
-  edit(i){
-    var todo= this.state.todo
-    this.setState({
-      editing:true,
-      value: this.state.todo[i].Value
-    });
-  };
-  savedit(i,keypress){
-    var Todo=this.refs.edittodo.value
-    var todo=this.state.todo
-    if( keypress.charCode == 13 )
-    {
-      this.setState({
-        editing:false,
-        todo:this.state.todo
-      });
-    };
-  };
-    rendernormal() {
-    return (
-        <div>
-          <h1 id='heading'>todos</h1>
-          <div className="lines"></div>
-          <div>
-            <input type="text" ref= "inputodo" onKeyPress={this.entertodo.bind(this)}className="inputodo"placeholder='todos'/>
-            <span onClick={this.allCompleted}id="all">^</span>
-          </div>
-          <div className="mainapp">
-            <ul className="decor">
-            {this.state.todo.map(this.todo.bind(this))}
-            </ul>
-          </div>
-        </div>
-      );
-    };
-    renderform(todo,i) {
-    return (
+
+  render(){
+    return(
       <div>
         <h1 id='heading'>todos</h1>
         <div className="lines"></div>
         <div>
-          <input type="text" onKeyPress={this.savedit.bind(this,i)} ref= "edittodo" value={this.state.value} onChange={this.changeValue.bind(this)} className="inputodo"placeholder='EDIT TODO'/>
+          <input type="text" ref= "inputodo" onKeyPress={this.entertodo.bind(this)}className="inputodo"placeholder='todos'/>
           <span onClick={this.allCompleted}id="all">^</span>
         </div>
         <div className="mainapp">
@@ -118,16 +81,7 @@ class App extends Component {
           </ul>
         </div>
       </div>
-      );
-    };
-    render(){
-      if (this.state.editing) {
-        return this.renderform()
-      }
-      else {
-      return this.rendernormal()
-      }
-    };
-  }
-
+    );
+  };
+};
   export default App;
